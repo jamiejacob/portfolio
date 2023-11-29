4cd3fa75c55f87bb87581150f3f820f0cd512ce7
jamiejacob <jamiejacob1988@gmail.com>
2023-11-29 11:34:25 +0100

    git changes

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import HomePage from "../components/HomePage";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <>
+      <div class="homepagebackground">
+        <Header></Header>
+        <HomePage></HomePage>
+      </div>
+    </>
+  );
+}
+
+export default Home;
